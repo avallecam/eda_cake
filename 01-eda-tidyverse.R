262b1c2b8c38f9245d2c58ea6f3e685c55aa65ca
Andree Valle Campos <avallecam@gmail.com>
2024-12-13 16:58:24 +0000

    add visualization features

--- 01-eda-tidyverse.R
+++ 01-eda-tidyverse.R
@@ -46,10 +46,18 @@ Gestation %>%
   rename(birthweight_oz = wt) %>% 
   # filter(smoke == "never" | smoke == "now") %>% 
   # mutate(smoke = str_to_title(smoke)) %>% 
-  mutate(smoke = fct_reorder(.f = smoke, .x = birthweight_oz, .fun = mean,.na_rm = TRUE)) %>% 
+  mutate(smoke = fct_reorder(.f = smoke, .x = birthweight_oz, .fun = mean, .na_rm = TRUE)) %>% 
   ggplot(aes(x = birthweight_oz)) +
   geom_histogram() +
+  facet_grid(vars(smoke)) +
+  scale_x_continuous(breaks = scales::pretty_breaks()) +
   geom_vline(data = dat_summary, aes(xintercept = weight_mean)) +
   geom_vline(data = dat_summary, aes(xintercept = weight_low), linetype = 3) +
   geom_vline(data = dat_summary, aes(xintercept = weight_high), linetype = 3) +
-  facet_grid(vars(smoke))
+  labs(
+    title = "Is birth weight associated with maternal smoking*?",
+    subtitle = "Data source: Child Health and Development Studies in 1961 and 1962.",
+    x = "Child birth weight (in ounces)",
+    y = "Frequency",
+    caption = "* Question in the survey: Does the mother smoke?"
+  )
